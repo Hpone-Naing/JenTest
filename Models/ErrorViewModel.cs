9f58409dbda8588d6bd6d06487ae9f5c6ae882ad
hponenaingtun <hponenaingtun@gmail.com>
2026-05-19 15:54:26 +0630

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
